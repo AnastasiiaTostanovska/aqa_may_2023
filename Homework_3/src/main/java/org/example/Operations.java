c7d52c924182f6fb9f726db8a840a286dda07888
AnastasiiaTostanovska <136075401+anastasiiatostanovska@users.noreply.github.com>
2023-06-16 03:24:15 +0300

    Homework_3

---
+++ Homework_3/src/main/java/org/example/Operations.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Operations {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 223;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int diff = a - b;
+        System.out.println(diff);
+
+        int mult = a * b;
+        System.out.println(mult);
+
+        float dev = (float) a / b;
+        System.out.println(dev);
+    }
+
+}
+
