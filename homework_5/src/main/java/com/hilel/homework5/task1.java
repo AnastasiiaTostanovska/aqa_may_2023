1664a2d2b54c0a870cd2ed5786675839789461be
Anastasiia Tostanovska <tostanovskaya21@gmail.com>
2023-06-26 12:54:48 +0300

    homework_5 changed task 1

--- homework_5/src/main/java/com/hilel/homework5/task1.java
+++ homework_5/src/main/java/com/hilel/homework5/task1.java
@@ -3,7 +3,7 @@ package com.hilel.homework5;
 
 public class task1 {
     public static void main(String[] args) {
-        for (int i = 1000; i <= 1015; i = i + 3) {
+        for (int i = 1000; i < 10000; i = i + 3) {
             System.out.println(i);
         }
     }
